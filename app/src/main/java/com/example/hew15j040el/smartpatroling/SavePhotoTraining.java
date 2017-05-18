087105f589820bd054c8bfe892d710fa4b566ba8
Luca <lucalazzaroni95@gmail.com>
2017-05-18 16:05:09 +0200

    a

--- app/src/main/java/com/example/hew15j040el/smartpatroling/SavePhotoTraining.java
+++ app/src/main/java/com/example/hew15j040el/smartpatroling/SavePhotoTraining.java
@@ -4,13 +4,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by HEW15J040EL on 10/05/2017.
@@ -30,6 +36,9 @@ public class SavePhotoTraining extends Activity {
     ImageView image;
 
 
+    private File tmpFile;
+    private Uri tmpFileURI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +47,26 @@ public class SavePhotoTraining extends Activity {
         //aprire la fotocamera
         context=getApplicationContext();
 
-        Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        camera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File()));
         startActivityForResult(camera,0);
+//        try
+//        {
+//            tmpFile = File.createTempFile("my_app", ".jpg");
+//        } catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        tmpFileURI = Uri.fromFile(tmpFile);
+
+//        File file =new File(Environment.getExternalStorageDirectory(),"file");
+//
+//        Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//
+//        camera.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,Uri.fromFile(file) );
+//        startActivityForResult(camera,0);//zero can be replaced with any action code
 
 
 
@@ -73,7 +100,7 @@ public class SavePhotoTraining extends Activity {
         });
 
         name=(TextView)findViewById(R.id.name);
-        name.setText("Write name:");
+        name.setText("Name:");
         writename=(EditText)findViewById(R.id.writename);
 
 //        bttCancel = (Button)findViewById(R.id.bttCancel);
@@ -100,6 +127,7 @@ public class SavePhotoTraining extends Activity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
     private void capturePhoto(View v){
         try {
             PhotoSaver2 ps = new PhotoSaver2(context,bp,_writename );
